ced9bd979bd6105305758d73753d6cf52212fe55
Roman Dubrovin <rdubrovin@ronasit.com>
2016-06-23 14:05:24 +0600

    #-: Added trait for TestCase.

--- tests/EncryptedTestCaseTrait.php
+++ tests/EncryptedTestCaseTrait.php
@@ -23,11 +23,28 @@ trait EncryptedTestCaseTrait
 
     public function call($method, $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null)
     {
+        /** @var EncryptService $encryptService */
+        $encryptService = app(EncryptService::class);
+
         if ($this->isEncrypted) {
-            $server['PUBLIC_KEY'] = '123';
-            $content = app(EncryptService::class)->encryptContent();
+            $publicKey = $encryptService->generatePublicKey();
+
+            $content = $encryptService->encryptContent($content, $publicKey);
+
+            $server['HTTP_PUBLIC_KEY'] = $publicKey;
         }
 
         parent::call($method, $uri, $parameters, $cookies, $files, $server, $content);
+    }
+
+    public function decryptResponse() {
+        /** @var EncryptService $encryptService */
+        $encryptService = app(EncryptService::class);
+
+        $publicKey = $this->response->headers->get('Public-Key');
+
+        $content = $encryptService->decryptContent($this->response->getContent(), $publicKey);
+
+        return response($content, $this->response->getStatusCode(), $this->response->headers->all());
     }
 }
